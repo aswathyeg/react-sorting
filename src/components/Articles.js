0e3af208438454b186f9f5474df5dcefdc7cd0a2
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-02-03 18:00:45 +0530

    added table

---
+++ src/components/Articles.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Articles = ({ articles }) => {
+  return (
+    <div className="card w-50 mx-auto">
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Upvotes</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        {articles.length &&
+          articles.map((art, i) => {
+            return (
+              <tbody key={i}>
+                <tr data-testid="article" key="article-index">
+                  <td data-testid="article-title">{art.title}</td>
+                  <td data-testid="article-upvotes">{art.upvotes}</td>
+                  <td data-testid="article-date">{art.date}</td>
+                </tr>
+              </tbody>
+            );
+          })}
+      </table>
+    </div>
+  );
+};
+
+export default Articles;
